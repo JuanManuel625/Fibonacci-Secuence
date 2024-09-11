d6bbbf31f4cdedef9d8b0f4dda4ec24a4c071ff0
JuanManuel625 <53581919+JuanManuel625@users.noreply.github.com>
2024-09-10 20:03:41 -0500

    Update Serie_Fibonnacci.java

--- Serie_Fibonnacci/src/serie_fibonnacci/Serie_Fibonnacci.java
+++ Serie_Fibonnacci/src/serie_fibonnacci/Serie_Fibonnacci.java
@@ -5,20 +5,38 @@ package serie_fibonnacci;
 
 import java.util.Scanner;
 public class Serie_Fibonnacci {
+    public class Serie_Fibonnacci {
     public static void main(String[] args) {
+        // Inicializamos las variables para la secuencia de Fibonacci
         int a=0, b=0, c=0, fin=0;
-        Scanner lee= new Scanner(System.in);
+        
+        // Creamos un objeto Scanner para leer la entrada del usuario
+        Scanner lee = new Scanner(System.in);
+        
+        // Solicitamos al usuario que ingrese el rango de la secuencia (cantidad de términos)
         System.out.println("Ingrese el rango de la secuencia");
-        fin=lee.nextInt();
+        fin = lee.nextInt(); // Leemos el número de términos
+        
+        // Solicitamos al usuario que ingrese el segundo valor inicial de la secuencia
+        // Esto permite personalizar el segundo valor (b) de la secuencia de Fibonacci
         System.out.println("Ingresa inicio de la secuencia");
-        b=lee.nextInt();
+        b = lee.nextInt(); // Leemos el segundo valor de la secuencia (b)
+        
+        // Comenzamos el bucle para generar y mostrar la secuencia de Fibonacci
         for (int i = 0; i < fin; i++) {
-            c=a+b;
-            a=b;
-            b=c;
-            System.out.print(","+c);
+            // Calculamos el siguiente número de Fibonacci sumando los dos anteriores
+            c = a + b;
+            // Actualizamos los valores de 'a' y 'b' para avanzar en la secuencia
+            a = b; // 'a' toma el valor de 'b'
+            b = c; // 'b' toma el valor de 'c' (el nuevo término)
+            
+            // Imprimimos el valor calculado de la secuencia
+            System.out.print("," + c);
         }
+        
+        // Salto de línea para finalizar la salida
         System.out.println("");
     }
+}
     
 }
